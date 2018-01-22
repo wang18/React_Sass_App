8592103a257483bf4a0bb8572305b6bd3f7c240d
wang18 <wang18@uoregon.edu>
2018-01-21 18:31:25 -0800

    add mapbox label

--- src/components/mapBox.js
+++ src/components/mapBox.js
@@ -6,7 +6,11 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmb
 
 class mapApp extends React.Component {
     map;
-
+    state = {
+        lng: 5,
+        lat: 34,
+        zoom: 1.5
+    };
     componentDidUpdate() {
         this.setFill();
     }
@@ -34,17 +38,27 @@ class mapApp extends React.Component {
             this.setFill();
         });
 
-        this.map.on('click',()=>{
-            const {lng,lat}=this.map.getCenter();
+        this.map.on('click', () => {
+            const {lng, lat} = this.map.getCenter();
 
             console.log(lng);
             console.log(lat);
 
         });
+
+        this.map.on('move', () => {
+            const { lng, lat } = this.map.getCenter();
+
+            this.setState({
+                lng: lng.toFixed(4),
+                lat: lat.toFixed(4),
+                zoom: this.map.getZoom().toFixed(2)
+            });
+        });
     }
 
     setFill() {
-        const { property, stops } = this.props.active;
+        const {property, stops} = this.props.active;
         this.map.setPaintProperty('countries', 'fill-color', {
             property,
             stops
@@ -52,8 +66,16 @@ class mapApp extends React.Component {
     }
 
     render() {
+        const { lng, lat, zoom } = this.state;
+
         return (
-            <div ref={el => this.mapContainer = el} className="absolute top right left bottom"/>
+            <div className="">
+                <div className="inline-block absolute top right mt12 ml12 bg-darken75 color-white z1 py6 px12 round-full txt-s txt-bold">
+                    <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div>
+                </div>
+
+                <div ref={el => this.mapContainer = el} className="absolute top right left bottom"/>
+            </div>
         );
     }
 }
